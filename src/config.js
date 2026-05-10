287c52f85c6e262b92cf843cd5824287d35d606c
rajritik21 <rajritik21av@gmail.com>
2026-05-10 12:56:51 +0530

    Fix API URL path

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const API_BASE_URL = process.env.NODE_ENV === 'production' 
-    ? 'https://task-manager-api-otuo.onrender.com'
-    : 'http://localhost:5001';
+    ? 'https://task-manager-api-otuo.onrender.com/api'
+    : 'http://localhost:5001/api';
 
 export default API_BASE_URL;
